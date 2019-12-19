e3ee895c4b061621cb75945d81dc2682e0115ce8
beckfordp <beckfordp@btinternet.com>
2019-12-19 10:25:05 +0000

    ATS-XX: Update following SKIRT merge.

--- app/services/TotalIncomeTaxService.scala
+++ app/services/TotalIncomeTaxService.scala
@@ -17,15 +17,10 @@
 package services
 
 import controllers.auth.AuthenticatedRequest
-import models.{AtsData, DataHolder, UserData}
-import uk.gov.hmrc.http.HeaderCarrier
-import utils.GenericViewModel
-import view_models.{Amount, Rate, TotalIncomeTax}
 import models.AtsData
 import uk.gov.hmrc.http.HeaderCarrier
 import utils.GenericViewModel
-import view_models.{Amount, Rate, SavingsRates, SavingsTax, ScottishRates, ScottishTax, TotalIncomeTax}
-
+import view_models._
 
 import scala.concurrent.Future
 
@@ -43,99 +38,90 @@ trait TotalIncomeTaxService {
   }
 
   private[services] def totalIncomeConverter(atsData: AtsData): TotalIncomeTax = {
-    val emptyAmount = Amount(0.0, "GBP")
-    val emptyRate = Rate("0")
-    val emptyUserData = UserData(Some(Map("title" -> "", "forename" -> "", "surname" ->" ")))
-    val incomeTaxData: DataHolder = atsData.income_tax.get
-    val rates: Map[String, Rate] = incomeTaxData.rates.getOrElse(Map())
-    val taxPayerData: UserData  = atsData.taxPayerData.getOrElse(emptyUserData)
 
+    def payload(key: String): Amount =
+      atsData.income_tax.flatMap(_.payload.flatMap(_.get(key))).getOrElse(Amount.empty)
 
+    def rates(key: String): Rate =
+      atsData.income_tax.flatMap(_.rates.flatMap(_.get(key))).getOrElse(Rate.empty)
 
+    def taxpayerName(key: String): String =
+      atsData.taxPayerData.flatMap(_.taxpayer_name.flatMap(_.get(key))).getOrElse("")
+
+    val scottishTax = ScottishTax(
+      payload("scottish_starter_rate_tax"),
+      payload("scottish_starter_income"),
+      payload("scottish_basic_rate_tax"),
+      payload("scottish_basic_income"),
+      payload("scottish_intermediate_rate_tax"),
+      payload("scottish_intermediate_income"),
+      payload("scottish_higher_rate_tax"),
+      payload("scottish_higher_income"),
+      payload("scottish_additional_rate_tax"),
+      payload("scottish_additional_income"),
+      payload("scottish_total_tax")
+    )
+
+    val savingsTax = SavingsTax(
+      payload("savings_lower_rate_tax"),
+      payload("savings_lower_income"),
+      payload("savings_higher_rate_tax"),
+      payload("savings_higher_income"),
+      payload("savings_additional_rate_tax"),
+      payload("savings_additional_income")
+    )
+
+    val scottishRates = ScottishRates(
+      rates("scottish_starter_rate"),
+      rates("scottish_basic_rate"),
+      rates("scottish_intermediate_rate"),
+      rates("scottish_higher_rate"),
+      rates("scottish_additional_rate")
+    )
+
+    val savingsRates = SavingsRates(
+      rates("savings_lower_rate"),
+      rates("savings_higher_rate"),
+      rates("savings_additional_rate")
+    )
+
+    TotalIncomeTax(
+      atsData.taxYear,
+      atsData.utr.getOrElse(""),
+      payload("starting_rate_for_savings"),
+      payload("starting_rate_for_savings_amount"),
+      payload("basic_rate_income_tax"),
+      payload("basic_rate_income_tax_amount"),
+      payload("higher_rate_income_tax"),
+      payload("higher_rate_income_tax_amount"),
+      payload("additional_rate_income_tax"),
+      payload("additional_rate_income_tax_amount"),
+      payload("ordinary_rate"),
+      payload("ordinary_rate_amount"),
+      payload("upper_rate"),
+      payload("upper_rate_amount"),
+      payload("additional_rate"),
+      payload("additional_rate_amount"),
+      payload("other_adjustments_increasing"),
+      payload("marriage_allowance_received_amount"),
+      payload("other_adjustments_reducing"),
+      scottishTax,
+      payload("total_income_tax"),
+      payload("scottish_income_tax"),
+      savingsTax,
+      atsData.income_tax.flatMap(_.incomeTaxStatus).getOrElse(""),
+      rates("starting_rate_for_savings_rate"),
+      rates("basic_rate_income_tax_rate"),
+      rates("higher_rate_income_tax_rate"),
+      rates("additional_rate_income_tax_rate"),
+      rates("ordinary_rate_tax_rate"),
+      rates("upper_rate_rate"),
+      rates("additional_rate_rate"),
+      scottishRates,
+      savingsRates,
+      taxpayerName("title"),
+      taxpayerName("forename"),
+      taxpayerName("surname")
+    )
   }
-      def payload(key: String): Amount =
-        atsData.income_tax.flatMap(_.payload.flatMap(_.get(key))).getOrElse(Amount.empty)
-
-      def rates(key: String): Rate =
-        atsData.income_tax.flatMap(_.rates.flatMap(_.get(key))).getOrElse(Rate.empty)
-
-      def taxpayerName(key: String): String =
-        atsData.taxPayerData.flatMap(_.taxpayer_name.flatMap(_.get(key))).getOrElse("")
-
-      val scottishTax = ScottishTax(
-        payload("scottish_starter_rate_tax"),
-        payload("scottish_starter_income"),
-        payload("scottish_basic_rate_tax"),
-        payload("scottish_basic_income"),
-        payload("scottish_intermediate_rate_tax"),
-        payload("scottish_intermediate_income"),
-        payload("scottish_higher_rate_tax"),
-        payload("scottish_higher_income"),
-        payload("scottish_additional_rate_tax"),
-        payload("scottish_additional_income"),
-        payload("scottish_total_tax")
-      )
-
-      val savingsTax = SavingsTax(
-        payload("savings_lower_rate_tax"),
-        payload("savings_lower_income"),
-        payload("savings_higher_rate_tax"),
-        payload("savings_higher_income"),
-        payload("savings_additional_rate_tax"),
-        payload("savings_additional_income")
-      )
-
-      val scottishRates = ScottishRates(
-        rates("scottish_starter_rate"),
-        rates("scottish_basic_rate"),
-        rates("scottish_intermediate_rate"),
-        rates("scottish_higher_rate"),
-        rates("scottish_additional_rate")
-      )
-
-      val savingsRates = SavingsRates(
-        rates("savings_lower_rate"),
-        rates("savings_higher_rate"),
-        rates("savings_additional_rate")
-      )
-
-      TotalIncomeTax(
-        atsData.taxYear,
-        atsData.utr.getOrElse(""),
-        payload("starting_rate_for_savings"),
-        payload("starting_rate_for_savings_amount"),
-        payload("basic_rate_income_tax"),
-        payload("basic_rate_income_tax_amount"),
-        payload("higher_rate_income_tax"),
-        payload("higher_rate_income_tax_amount"),
-        payload("additional_rate_income_tax"),
-        payload("additional_rate_income_tax_amount"),
-        payload("ordinary_rate"),
-        payload("ordinary_rate_amount"),
-        payload("upper_rate"),
-        payload("upper_rate_amount"),
-        payload("additional_rate"),
-        payload("additional_rate_amount"),
-        payload("other_adjustments_increasing"),
-        payload("marriage_allowance_received_amount"),
-        payload("other_adjustments_reducing"),
-        scottishTax,
-        payload("total_income_tax"),
-        payload("scottish_income_tax"),
-        savingsTax,
-        atsData.income_tax.flatMap(_.incomeTaxStatus).getOrElse(""),
-        rates("starting_rate_for_savings_rate"),
-        rates("basic_rate_income_tax_rate"),
-        rates("higher_rate_income_tax_rate"),
-        rates("additional_rate_income_tax_rate"),
-        rates("ordinary_rate_tax_rate"),
-        rates("upper_rate_rate"),
-        rates("additional_rate_rate"),
-        scottishRates,
-        savingsRates,
-        taxpayerName("title"),
-        taxpayerName("forename"),
-        taxpayerName("surname")
-      )
-    }
 }
