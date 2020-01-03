575da4c2408371eee584bc180c8d4ab544fd76a5
barrida <suleymanube@gmail.com>
2020-01-03 09:30:51 +0000

    make paye tax year parametric

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 HM Revenue & Customs
+ * Copyright 2020 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,7 +90,7 @@ trait IndexController extends TaxsController {
 
   override def show(implicit request: AuthenticatedRequest[_]): Future[Result] = {
     (request.saUtr, request.nino) match {
-      case (None, Some(_)) =>  Future(Redirect(routes.AtsMainController.authorisedAtsMain().url + "?taxYear=2019"))
+      case (None, Some(_)) =>  Future(Redirect(routes.AtsMainController.authorisedAtsMain().url + "?taxYear=" + Globals.TAXS_PAYE_YEAR))
       case (Some(_), _)   =>  showAvailableTaxYears(request)
     }
   }
--- app/utils/Globals.scala
+++ app/utils/Globals.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 HM Revenue & Customs
+ * Copyright 2020 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,4 +30,5 @@ object Globals {
   val TAXS_ANALYTICS_AGENT_ATTRIBUTE = "annual-tax-summary:agent:start"
   val TAXS_ANALYTICS_PORTAL_USER_ATTRIBUTE = "annual-tax-summary:portal-user:start"
   val TAXS_ANALYTICS_TRANSITIONED_USER_ATTRIBUTE = "annual-tax-summary:transitioned-user:start"
+  val TAXS_PAYE_YEAR = 2019
 }
