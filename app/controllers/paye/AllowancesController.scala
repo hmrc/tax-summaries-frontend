d59a1427101ad8b7aa1e8271ac1be3dbeb880360
beckfordp <beckfordp@btinternet.com>
2020-02-03 13:05:48 +0000

    Temp refactor

---
+++ app/controllers/paye/AllowancesController.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.paye
+
+import config.AppFormPartialRetriever
+import controllers.auth.{AuthAction, AuthenticatedRequest}
+import models.ErrorResponse
+import play.api.Play
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+import play.api.mvc.Result
+import services.{AllowanceService, AuditService}
+import uk.gov.hmrc.play.partials.FormPartialRetriever
+import utils.GenericViewModel
+import view_models.Allowances
+
+import scala.concurrent.Future
+
+object AllowancesController extends AllowancesController {
+  override val allowanceService = AllowanceService
+  override val auditService = AuditService
+  override val formPartialRetriever = AppFormPartialRetriever
+  override val authAction = Play.current.injector.instanceOf[AuthAction]
+}
+
+trait AllowancesController extends TaxYearRequest {
+
+  implicit val formPartialRetriever: FormPartialRetriever
+
+  val authAction: AuthAction
+
+  def allowanceService: AllowanceService
+
+  def authorisedAllowance = authAction.async {
+    request => show(request)
+  }
+
+  type ViewModel = Allowances
+
+  override def extractViewModel()(implicit request: AuthenticatedRequest[_]): Future[Either[ErrorResponse,GenericViewModel]] = {
+    extractViewModelWithTaxYear(allowanceService.getAllowances(_))
+  }
+
+  override def obtainResult(result: ViewModel)(implicit request: AuthenticatedRequest[_]): Result = {
+    Ok(views.html.tax_free_amount(result, getActingAsAttorneyFor(request, result.forename, result.surname, result.utr)))
+  }
+
+}
---
+++ app/controllers/paye/SummaryController.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.paye
+
+import config.AppFormPartialRetriever
+import controllers.auth.{AuthAction, AuthenticatedRequest}
+import models.ErrorResponse
+import play.api.Play
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+import play.api.mvc.Result
+import services.{AuditService, SummaryService}
+import uk.gov.hmrc.play.partials.FormPartialRetriever
+import utils.GenericViewModel
+import view_models.Summary
+
+import scala.concurrent.Future
+
+object SummaryController extends SummaryController{
+  override val summaryService = SummaryService
+  override val auditService = AuditService
+  override val formPartialRetriever = AppFormPartialRetriever
+  override val authAction = Play.current.injector.instanceOf[AuthAction]
+}
+
+trait SummaryController extends TaxYearRequest {
+
+  implicit val formPartialRetriever: FormPartialRetriever
+
+  val authAction: AuthAction
+
+  def summaryService: SummaryService
+
+  def authorisedSummaries = authAction.async {
+    request => show(request)
+  }
+
+  type ViewModel = Summary
+
+  override def extractViewModel()(implicit request: AuthenticatedRequest[_]): Future[Either[ErrorResponse,GenericViewModel]] = {
+     extractViewModelWithTaxYear(summaryService.getSummaryData(_))
+  }
+
+  override def obtainResult(result: ViewModel)(implicit request: AuthenticatedRequest[_]): Result = {
+    Ok(views.html.summary(result, getActingAsAttorneyFor(request, result.forename, result.surname, result.utr)))
+  }
+}
