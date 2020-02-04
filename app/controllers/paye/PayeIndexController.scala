fcd1cb523239914f1e547478d9f99b66187b260d
beckfordp <beckfordp@btinternet.com>
2020-02-04 10:42:57 +0000

    WIP

--- app/controllers/paye/PayeIndexController.scala
+++ app/controllers/paye/PayeIndexController.scala
@@ -16,151 +16,34 @@
 
 package controllers.paye
 
-import java.util.Date
-
 import config.{AppFormPartialRetriever, ApplicationConfig}
 import connectors.DataCacheConnector
 import controllers.auth.{AuthAction, AuthenticatedRequest}
-import models.ErrorResponse
-import play.Logger
 import play.api.Play
-import play.api.Play.current
-import play.api.i18n.Messages.Implicits._
 import play.api.mvc.Result
-import services.{AtsListService, AtsYearListService, AuditService, AuditTypes}
+import uk.gov.hmrc.play.frontend.controller.FrontendController
 import uk.gov.hmrc.play.partials.FormPartialRetriever
-import utils._
-import view_models.AtsForms._
-import view_models.{AtsList, NoATSViewModel, TaxYearEnd}
 
 import scala.concurrent.Future
 
-object IndexController extends IndexController {
-  override val atsYearListService = AtsYearListService
-  override val auditService = AuditService
-  override lazy val dataCache = DataCacheConnector
-  override val atsListService = AtsListService
+object PayeIndexController extends PayeIndexController {
   override val formPartialRetriever = AppFormPartialRetriever
   override val authAction = Play.current.injector.instanceOf[AuthAction]
+  override lazy val dataCache = DataCacheConnector
 }
 
-trait IndexController extends TaxsController {
+trait PayeIndexController extends FrontendController {
 
   implicit val formPartialRetriever: FormPartialRetriever
 
   val authAction: AuthAction
-
   val dataCache: DataCacheConnector
-  def atsYearListService: AtsYearListService
-  def atsListService: AtsListService
 
   def authorisedIndex = authAction.async {
-    request => agentAwareShow(request)
+    request => show(request)
   }
 
-  def authorisedOnSubmit = authAction.async {
-    request => onSubmit(request)
-  }
-
-  //FIXME add extra check - agent tries multiple ids in same session
-  def agentAwareShow(implicit request: AuthenticatedRequest[_]): Future[Result] =
-
-    request.getQueryString(Globals.TAXS_USER_TYPE_QUERY_PARAMETER) match {
-      case Some(Globals.TAXS_PORTAL_REFERENCE) => {
-
-        val session = request.session + (Globals.TAXS_USER_TYPE_KEY -> Globals.TAXS_PORTAL_REFERENCE)
-        val agentToken = request.getQueryString(Globals.TAXS_AGENT_TOKEN_ID)
-
-        agentToken.fold[Future[_]] {
-          Future.successful(None)
-        } { token =>
-          if (AccountUtils.isAgent(request)) {
-            dataCache.storeAgentToken(token) recover { case e: Throwable => throw e }
-          } else {
-            Future.successful(None)
-          }
-        } map {
-          x => Redirect(routes.IndexController.authorisedIndex()).withSession(session)
-        }
-      }
-      case _ => {
-        show(request)
-      }
-    }
-
-  override def show(implicit request: AuthenticatedRequest[_]): Future[Result] = {
-    (request.saUtr, request.agentRef, request.nino) match {
-      case (None, None, Some(_)) =>  Future(Redirect(routes.AtsMainController.authorisedAtsMain().url + "?taxYear=" + ApplicationConfig.payeTaxYear))
-      case (_, _, _)   =>  showAvailableTaxYears(request)
-    }
-  }
-
-  def showAvailableTaxYears(implicit request: AuthenticatedRequest[_]): Future[Result] = {
-    transformation recover {
-      case error =>
-        Logger.info(Globals.TAXS_LOGGER_ERROR_DESCR, error)
-        error match {
-          case token_error: AgentTokenException =>
-            auditService.sendEvent(AuditTypes.Tx_FAILED, Map(
-              "userId" -> getAccountId(request), "error" -> token_error.message,
-              "time" -> new Date().toString,
-              "attemptedToken" -> request2flash.get(Globals.TAXS_AGENT_TOKEN_KEY).getOrElse("")))
-            Ok(views.html.errors.token_error())
-          case _ => Ok(views.html.errors.generic_error())
-        }
-    }
-  }
-
-  type ViewModel = AtsList
-
-
-  override def extractViewModel()(implicit request: AuthenticatedRequest[_]): Future[Either[ErrorResponse,GenericViewModel]] = {
-      atsYearListService.getAtsListData.map(Right(_))
-  }
-
-  def getViewModel(result: ViewModel)(implicit request: AuthenticatedRequest[_]): Future[Result] = {
-    result.yearList match {
-      case TaxYearEnd(year) :: Nil => redirectWithYear(year.get.toInt)
-      case _ => Future.successful(Ok(views.html.taxs_index(result, atsYearFormMapping, getActingAsAttorneyFor(request, result.forename, result.surname, result.utr))).withSession(request.session + ("atsList" -> result.toString)))
-    }
-  }
-
-  override def transformation(implicit request: AuthenticatedRequest[_]): Future[Result] = {
-    extractViewModel flatMap {
-      case Right(noATS: NoATSViewModel) => Future.successful(Redirect(routes.ErrorController.authorisedNoAts()))
-      case Right(result: ViewModel) => getViewModel(result)
-    }
-  }
-
-  def onSubmit(implicit request: AuthenticatedRequest[_]): Future[Result] = {
-    atsYearFormMapping.bindFromRequest.fold(
-      formWithErrors => {
-        val session = request.session + (Globals.TAXS_USER_TYPE_KEY -> Globals.TAXS_PORTAL_REFERENCE)
-        atsListService.getAtsYearList flatMap {
-          atsListData => {
-            val atsList = new AtsList(atsListData.utr,
-              atsListData.taxPayer.get.taxpayer_name.get("forename"),
-              atsListData.taxPayer.get.taxpayer_name.get("surname"),
-              atsListData.atsYearList.get.map(year => TaxYearEnd(Some(year.toString))))
-            Future.successful(Ok(views.html.taxs_index(atsList, formWithErrors)).withSession(session))
-          }
-        }
-      },
-      value => redirectWithYear(value.year.get.toInt)
-    )
-  }
-
-  def redirectWithYear(year: Int)(implicit request: AuthenticatedRequest[_]): Future[Result] = {
-      atsListService.getAtsYearList flatMap {
-        atsListData => {
-          val taxYearListLength = atsListData.atsYearList.get.map(year => TaxYearEnd(Some(year.toString))).length
-          Future(Redirect(routes.AtsMainController.authorisedAtsMain().url + "?taxYear=" + year).withSession(request.session + ("TaxYearListLength" -> taxYearListLength.toString)))
-        }
-      }
-  }
-
-  // This is unused, it is only implemented to adhere to the interface
-  override def obtainResult(result: ViewModel)(implicit request: AuthenticatedRequest[_]): Result = {
-    Ok(views.html.taxs_index(result, atsYearFormMapping, getActingAsAttorneyFor(request, result.forename, result.surname, result.utr)))
+  def show(implicit request: AuthenticatedRequest[_]): Future[Result] = {
+    Future(Redirect(routes.PayeAtsMainController.authorisedAtsMain().url + "?taxYear=" + ApplicationConfig.payeTaxYear) )
   }
 }
---
+++ test/controllers/paye/PayeAtsMainControllerSpec.scala
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.paye
+
+import config.AppFormPartialRetriever
+import controllers.auth.{AuthAction, AuthenticatedRequest, FakeAuthAction}
+import org.jsoup.Jsoup
+import org.mockito.Matchers
+import org.mockito.Mockito.when
+import org.scalatest.MustMatchers._
+import org.scalatest.mockito.MockitoSugar
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services._
+import uk.gov.hmrc.domain.{Nino, SaUtr}
+import uk.gov.hmrc.play.partials.FormPartialRetriever
+import uk.gov.hmrc.play.test.UnitSpec
+import utils.TestConstants._
+import view_models.{Amount, NoATSViewModel, Rate, Summary}
+
+import scala.concurrent.Future
+
+class PayeAtsMainControllerSpec extends UnitSpec with GuiceOneAppPerSuite with MockitoSugar with I18nSupport {
+
+  override def messagesApi: MessagesApi = fakeApplication.injector.instanceOf[MessagesApi]
+
+  val taxYear = 2014
+   val baseModel = Summary(
+    year = 2014,
+    utr = testUtr,
+    employeeNicAmount = Amount(1200, "GBP"),
+    employerNicAmount = Amount(1300, "GBP"),
+    totalIncomeTaxAndNics = Amount(1400, "GBP"),
+    yourTotalTax = Amount(1800, "GBP"),
+    totalTaxFree = Amount(9440, "GBP"),
+    totalTaxFreeAllowance = Amount(9740, "GBP"),
+    yourIncomeBeforeTax = Amount(11600, "GBP"),
+    totalIncomeTaxAmount = Amount(372, "GBP"),
+    totalCapitalGainsTax = Amount(5500, "GBP"),
+    taxableGains = Amount(20000, "GBP"),
+    cgTaxPerCurrencyUnit = Amount(0.1234, "GBP"),
+    nicsAndTaxPerCurrencyUnit = Amount(0.5678, "GBP"),
+    nicsAndTaxRateAmount = Amount(0.5678, "GBP"),
+    totalCgTaxRate = Rate("12.34%"),
+    nicsAndTaxRate = Rate("56.78%"),
+    incomeAfterTaxAndNics = Amount(0.5678, "GBP"),
+    title = "Mr",
+    forename = "forename",
+    surname = "surname"
+  )
+  val request = AuthenticatedRequest("userId", None, None, Some(Nino(testNino)), None, None, None, FakeRequest("GET", s"?taxYear=$taxYear"))
+  val badRequest = AuthenticatedRequest("userId", None, None, Some(Nino(testNino)), None, None, None, FakeRequest("GET","?taxYear=20145"))
+
+  trait TestController extends PayeAtsMainController {
+    override lazy val summaryService = mock[SummaryService]
+    override lazy val auditService = mock[AuditService]
+    implicit lazy val formPartialRetriever: FormPartialRetriever = AppFormPartialRetriever
+    override val authAction: AuthAction = FakeAuthAction
+    when(summaryService.getSummaryData(Matchers.eq(taxYear))(Matchers.any(), Matchers.eq(request))).thenReturn(Future.successful(baseModel))
+
+  }
+
+  "Calling Index Page" should {
+
+    "return a successful response for a valid request" in new TestController {
+      val result =  Future.successful(show(request))
+      status(result) shouldBe 200
+      val document = Jsoup.parse(contentAsString(result))
+      document.title should include(Messages("ats.index.html.title")+ Messages("generic.to_from", (taxYear-1).toString, taxYear.toString))
+    }
+
+    "display an error page for an invalid request" in new TestController {
+      val result = Future.successful(show(badRequest))
+      status(result) shouldBe 400
+      val document = Jsoup.parse(contentAsString(result))
+      document.title should include(Messages("generic.error.html.title"))
+    }
+
+    "redirect to the no ATS page when there is no Annual Tax Summary data returned" in new TestController {
+
+      when(summaryService.getSummaryData(Matchers.eq(taxYear))(Matchers.any(), Matchers.eq(request))).thenReturn(Future.successful(new NoATSViewModel))
+
+      val result = Future.successful(show(request))
+      status(result) mustBe SEE_OTHER
+
+      redirectLocation(result).get mustBe controllers.routes.ErrorController.authorisedNoAts().url
+
+    }
+
+    "have the right user data in the view" in new TestController {
+
+      val result = Future.successful(show(request))
+      val document = Jsoup.parse(contentAsString(result))
+
+      status(result) shouldBe 200
+      document.getElementById("tax-calc-link").text shouldBe "Your income and taxes"
+      document.getElementById("tax-services-link").text shouldBe "Your taxes and public spending"
+      document.getElementById("index-page-header").text shouldBe "Tax Year: 6 April 2013 to 5 April 2014 Your Annual Tax Summary"
+      document.getElementById("index-page-description").text shouldBe "This summarises your personal tax and National Insurance, and how they are spent by government. This information comes from you, your employer(s) or your pension provider(s)."
+      document.getElementById("tax-calc-link").tagName shouldBe "a"
+      document.getElementById("tax-services-link").tagName shouldBe "a"
+      document.getElementById("user-info").text should include("forename surname")
+      document.getElementById("user-info").text should include("Unique Taxpayer Reference: "+testUtr)
+    }
+
+    "display the right years" in new TestController {
+
+      val model = baseModel.copy(
+        year = 2015
+      )
+
+      when(summaryService.getSummaryData(Matchers.eq(taxYear))(Matchers.any(), Matchers.eq(request))).thenReturn(Future.successful(model))
+
+      val result = Future.successful(show(request))
+      val document = Jsoup.parse(contentAsString(result))
+
+      status(result) shouldBe 200
+      document.getElementById("index-page-header").text should include("2015")
+    }
+
+    "show 'Landing page' page with a correct breadcrumb" in new TestController {
+
+      val result = Future.successful(show(request))
+      val document = Jsoup.parse(contentAsString(result))
+
+      document.select("#global-breadcrumb li:nth-child(1) a").attr("href") should include("/account")
+      document.select("#global-breadcrumb li:nth-child(1) a").text should include("Home")
+
+      document.select("#global-breadcrumb li:nth-child(2)").toString should include("Select the tax year")
+    }
+  }
+}
