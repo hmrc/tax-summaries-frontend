bda04e89e86fb09458ef9ad707ce2049ad02f2b3
Chris Wright <chrisjameswright@users.noreply.github.com>
2020-02-21 22:10:31 +0000

    Adding assertions to test all spending rows

--- test/controllers/paye/PayeGovernmentSpendControllerSpec.scala
+++ test/controllers/paye/PayeGovernmentSpendControllerSpec.scala
@@ -33,8 +33,6 @@ import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
 import uk.gov.hmrc.play.test.UnitSpec
 import utils.JsonUtil
 import utils.TestConstants.testNino
-import view_models.Amount
-import view_models.paye.{PayeGovernmentSpend, SpendRow}
 
 import scala.concurrent.Future
 import scala.io.Source
@@ -44,32 +42,14 @@ class PayeGovernmentSpendControllerSpec  extends UnitSpec with MockitoSugar with
   implicit val hc = HeaderCarrier()
   override def messagesApi: MessagesApi = fakeApplication.injector.instanceOf[MessagesApi]
 
-  val taxYear = 2020
-  val fakeAuthenticatedRequest = PayeAuthenticatedRequest("userId", testNino, FakeRequest("GET", s"?taxYear=$taxYear"))
-
-  val expectedViewModel =  PayeGovernmentSpend(2019, List(
-    SpendRow("welfare", 24.52, Amount(5863.22, "GBP")),
-    SpendRow("health", 18.87, Amount(4512.19, "GBP")),
-    SpendRow("pension", 12.12, Amount(2898.13, "GBP")),
-    SpendRow("education", 13.15, Amount(3144.43, "GBP")),
-    SpendRow("defence", 5.31, Amount(1269.73, "GBP")),
-    SpendRow("national_debt_interest", 7.00, Amount(1673.84, "GBP")),
-    SpendRow("transport", 2.95, Amount(705.4, "GBP")),
-    SpendRow("criminal_justice", 4.40, Amount(1052.13, "GBP")),
-    SpendRow("business_and_industry", 2.74, Amount(655.19, "GBP")),
-    SpendRow("government_administration", 2.05, Amount(490.2, "GBP")),
-    SpendRow("housing_and_utilities", 1.64, Amount(392.16, "GBP")),
-    SpendRow("environment", 1.66, Amount(396.94, "GBP")),
-    SpendRow("culture", 1.69, Amount(404.11, "GBP")),
-    SpendRow("overseas_aid", 1.15, Amount(274.99, "GBP")),
-    SpendRow("uk_contribution_to_eu_budget", 0.75, Amount(179.34, "GBP")))
-    , totalAmount = Amount(4512.00,"GBP"))
+  val taxYear = 2019
+  val fakeAuthenticatedRequest = PayeAuthenticatedRequest("userId", testNino, FakeRequest("GET", "/annual-tax-summary/paye/treasury-spending"))
 
   class TestController extends PayeGovernmentSpendController {
 
     override val payeAuthAction: PayeAuthAction = FakePayeAuthAction
     override val payeAtsService = mock[PayeAtsService]
-    override val payeYear = 2019
+    override val payeYear = taxYear
 
     private def readJson(path: String) = {
       val resource = getClass.getResourceAsStream(path)
@@ -92,7 +72,7 @@ class PayeGovernmentSpendControllerSpec  extends UnitSpec with MockitoSugar with
 
       val document = Jsoup.parse(contentAsString(result))
 
-      document.title should include(Messages("paye.ats.treasury_spending.title")+ Messages("generic.to_from", (taxYear-1).toString, taxYear.toString))
+      document.title should include(Messages("paye.ats.treasury_spending.title")+ Messages("generic.to_from", taxYear.toString, (taxYear + 1).toString))
     }
 
     "have correct data for 2019" in new TestController {
@@ -105,36 +85,53 @@ class PayeGovernmentSpendControllerSpec  extends UnitSpec with MockitoSugar with
       status(result) shouldBe OK
 
       val document = Jsoup.parse(contentAsString(result))
-      document.getElementById("welfare").text() shouldBe "Welfare (24.52%)"
-//      document.getElementById("health").text() shouldBe "Health (18.87%)"
-//      document.select("#health").text() should include("18.87%")
-//      document.select("#education + td").text() shouldBe "£3,144.43"
-//      document.select("#education").text() should include("13.15%")
-//      document.select("#pension + td").text() shouldBe "£2,898.13"
-//      document.select("#pension").text() should include("12.12%")
-//      document.select("#national_debt_interest + td").text() shouldBe "£1,673.84"
-//      document.select("#national_debt_interest").text() should include("7.0%")
-//      document.select("#defence + td").text() shouldBe "£1,269.73"
-//      document.select("#defence").text() should include("5.31%")
-//      document.select("#criminal_justice + td").text() shouldBe "£1,052.13"
-//      document.select("#criminal_justice").text() should include("4.4%")
-//      document.select("#transport + td").text() shouldBe "£705.40"
-//      document.select("#transport").text() should include("2.95%")
-//      document.select("#business_and_industry + td").text() shouldBe "£655.19"
-//      document.select("#business_and_industry").text() should include("2.74%")
-//      document.select("#government_administration + td").text() shouldBe "£490.20"
-//      document.select("#government_administration").text() should include("2.05%")
-//      document.select("#Culture + td").text() shouldBe "£404.11"
-//      document.select("#Culture").text() should include("1.69%")
-//      document.select("#Environment + td").text() shouldBe "£396.94"
-//      document.select("#Environment").text() should include("1.66%")
-//      document.select("#HousingAndUtilities + td").text() shouldBe "£392.16"
-//      document.select("#HousingAndUtilities").text() should include("1.64%")
-//      document.select("#overseas_aid + td").text() shouldBe "£274.99"
-//      document.select("#overseas_aid").text() should include("1.15%")
-//      document.select("#uk_contribution_to_eu_budget + td").text() shouldBe "£179.34"
-//      document.select("#uk_contribution_to_eu_budget").text() should include("0.75%")
-//      document.select("#gov-spend-total + td").text() shouldBe "£23,912.00"
+      document.getElementById("Welfare").text() shouldBe "Welfare (24.52%)"
+      document.select("#Welfare + dd").text() shouldBe "£5,863"
+
+      document.getElementById("Health").text() shouldBe "Health (18.87%)"
+      document.select("#Health + dd").text() shouldBe "£4,512"
+
+      document.getElementById("StatePensions").text() shouldBe "State Pensions (12.12%)"
+      document.select("#StatePensions + dd").text() shouldBe "£2,898"
+
+      document.getElementById("Education").text() shouldBe "Education (13.15%)"
+      document.select("#Education + dd").text() shouldBe "£3,144"
+
+      document.getElementById("Defence").text() shouldBe "Defence (5.31%)"
+      document.select("#Defence + dd").text() shouldBe "£1,269"
+
+      document.getElementById("NationalDebtInterest").text() shouldBe "National Debt Interest (7.0%)"
+      document.select("#NationalDebtInterest + dd").text() shouldBe "£1,673"
+
+      document.getElementById("Transport").text() shouldBe "Transport (2.95%)"
+      document.select("#Transport + dd").text() shouldBe "£705"
+
+      document.getElementById("PublicOrderAndSafety").text() shouldBe "Public Order and Safety (4.4%)"
+      document.select("#PublicOrderAndSafety + dd").text() shouldBe "£1,052"
+
+      document.getElementById("BusinessAndIndustry").text() shouldBe "Business and Industry (2.74%)"
+      document.select("#BusinessAndIndustry + dd").text() shouldBe "£655"
+
+      document.getElementById("GovernmentAdministration").text() shouldBe "Government Administration (2.05%)"
+      document.select("#GovernmentAdministration + dd").text() shouldBe "£490"
+
+      document.getElementById("HousingAndUtilities").text() shouldBe "Housing and Utilities, like street lighting (1.64%)"
+      document.select("#HousingAndUtilities + dd").text() shouldBe "£392"
+
+      document.getElementById("Environment").text() shouldBe "Environment (1.66%)"
+      document.select("#Environment + dd").text() shouldBe "£396"
+
+      document.getElementById("Culture").text() shouldBe "Culture, like sports, libraries, museums (1.69%)"
+      document.select("#Culture + dd").text() shouldBe "£404"
+
+      document.getElementById("OverseasAid").text() shouldBe "Overseas Aid (1.15%)"
+      document.select("#OverseasAid + dd").text() shouldBe "£274"
+
+      document.getElementById("UkContributionToEuBudget").text() shouldBe "UK Contribution to the EU Budget (0.75%)"
+      document.select("#UkContributionToEuBudget + dd").text() shouldBe "£179"
+
+      document.select("#TotalAmount + dd").text() shouldBe "£4,512"
+
       document
         .select("h1")
         .text shouldBe "How your tax was spent 6 April 2019 to 5 April 2020"
