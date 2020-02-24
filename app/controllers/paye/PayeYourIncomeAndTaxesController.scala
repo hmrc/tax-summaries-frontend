c9f463dc9f1f10ca78fd6e86e0d4e3cead4b26b2
Michael Kitchen <kitcheonon@hotmail.com>
2020-02-24 13:49:23 +0000

    ATS-99 | Added basic controller for your income and taxes

---
+++ app/controllers/paye/PayeYourIncomeAndTaxesController.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.paye
+
+import config.AppFormPartialRetriever
+import controllers.auth.{PayeAuthAction, PayeAuthenticatedRequest}
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import view_models.Amount
+import view_models.paye.PayeYourIncomeAndTaxes
+
+import scala.concurrent.Future
+
+
+trait PayeYourIncomeAndTaxesController extends FrontendController {
+
+  implicit val formPartialRetriever = AppFormPartialRetriever
+
+  val payeAuthAction: PayeAuthAction
+  val payeYear: Int
+
+  def show: Action[AnyContent] = payeAuthAction.async {
+    implicit request: PayeAuthenticatedRequest[_] => {
+
+      val blankViewModel = PayeYourIncomeAndTaxes(
+        taxYear = 2019,
+        employeeContributions = false,
+        incomeBeforeTax = Amount(200, "GBP"),
+        taxableIncome = Amount(200, "GBP"),
+        totalIncomeTax = Amount(200, "GBP"),
+        totalIncomeTaxNics = Amount(200, "GBP"),
+        incomeAfterTaxNics = Amount(200, "GBP"),
+        averageTaxRate = Amount(200, "GBP"),
+        taxFreeAmount =  Amount(200, "GBP"))
+
+      Future.successful(Ok(views.html.paye.paye_your_income_and_taxes(blankViewModel)))
+
+
+    }
+
+  }
+}
---
+++ test/controllers/paye/PayeYourIncomeAndTaxesControllerSpec.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.paye
+
+import controllers.auth.{FakePayeAuthAction, PayeAuthAction, PayeAuthenticatedRequest}
+import models.PayeAtsData
+import org.jsoup.Jsoup
+import org.mockito.Matchers.{any, eq => eqTo}
+import org.mockito.Mockito.when
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.mockito.MockitoSugar
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.libs.json.{JsValue, Json}
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout, redirectLocation}
+import services.PayeAtsService
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.play.test.UnitSpec
+import utils.JsonUtil
+import utils.TestConstants.testNino
+
+import scala.concurrent.Future
+import scala.io.Source
+
+class PayeYourIncomeAndTaxesControllerSpec  extends UnitSpec with MockitoSugar with JsonUtil with GuiceOneAppPerTest with ScalaFutures with I18nSupport with IntegrationPatience {
+
+  implicit val hc = HeaderCarrier()
+  override def messagesApi: MessagesApi = fakeApplication.injector.instanceOf[MessagesApi]
+
+  val taxYear = 2019
+  val fakeAuthenticatedRequest = PayeAuthenticatedRequest("userId", testNino, FakeRequest("GET", "/annual-tax-summary/paye/treasury-spending"))
+
+  class TestController extends PayeYourIncomeAndTaxesController {
+
+    override val payeAuthAction: PayeAuthAction = FakePayeAuthAction
+    override val payeYear = taxYear
+
+    private def readJson(path: String) = {
+      val resource = getClass.getResourceAsStream(path)
+      Json.parse(Source.fromInputStream(resource).getLines().mkString)
+    }
+
+    val expectedResponse: JsValue = readJson("/paye_ats.json")
+  }
+
+  "Government spend controller" should {
+
+    "return OK response" in new TestController {
+
+      val result = show(fakeAuthenticatedRequest)
+
+      status(result) shouldBe OK
+    }
+  }
+
+}
