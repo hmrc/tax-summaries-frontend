b9936e71b003d4f17ce838ba66a31ef7419198b7
Andrew Arkless <14139642+AndrewArkless@users.noreply.github.com>
2020-07-07 14:18:51 +0100

    DDCNLS-2025 when negative rate display 0 instead of negative number

--- test/utils/view_utils/ViewUtilsSpec.scala
+++ test/utils/view_utils/ViewUtilsSpec.scala
@@ -19,11 +19,12 @@ package utils.view_utils
 import org.scalatest.prop.PropertyChecks
 import uk.gov.hmrc.play.test.UnitSpec
 import utils.ViewUtils
-import view_models.Amount
+import view_models.{Amount, Rate}
 
 class ViewUtilsSpec extends UnitSpec with PropertyChecks {
 
   val zeroAmount = createAmount(0)
+  val zeroRate=Rate("0%")
 
   def createAmount(bd: BigDecimal) = Amount(bd, "gbp")
 
@@ -63,12 +64,36 @@ class ViewUtilsSpec extends UnitSpec with PropertyChecks {
     }
 
     "return an amount object with zero as the amount if the given amount is negative" in {
-      forAll { dec: BigDecimal =>
+      forAll { dec: Double =>
         whenever(dec < 0) {
           val amount = createAmount(dec)
           ViewUtils.positiveOrZero(amount) shouldBe zeroAmount
         }
       }
     }
+
+    "return the given rate if the percentage is positive" in {
+      forAll{ dec: Double=>
+        whenever(dec>0){
+          val percentage=dec.toString+'%'
+          val positiveRate=Rate(percentage)
+          ViewUtils.positiveOrZero(positiveRate) shouldBe positiveRate
+        }
+      }
+    }
+
+    "return the zero rate if the percentage is zero" in {
+      ViewUtils.positiveOrZero(zeroRate) shouldBe Rate.empty
+    }
+
+    "return the zero rate if the percentage is negative" in{
+      forAll{dec:Double=>
+        whenever(dec<0) {
+          val percentage = dec.toString + '%'
+          val negativeRate = Rate(percentage)
+          ViewUtils.positiveOrZero(negativeRate) shouldBe Rate.empty
+        }
+        }
+      }
   }
 }
