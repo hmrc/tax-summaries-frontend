4e5c075c97e65100a0a694806c1e4a49107ba573
muhammad <muhammad.ahmad@digital.hmrc.gov.uk>
2021-02-01 16:14:01 +0000

    tests updated

--- test/controllers/NicsSummaryControllerSpec.scala
+++ test/controllers/NicsSummaryControllerSpec.scala
@@ -140,22 +140,21 @@ class NicsSummaryControllerSpec extends ControllerBaseSpec with BeforeAndAfterEa
       val result = Future.successful(sut.show(request))
       val document = Jsoup.parse(contentAsString(result))
 
-      document.select("#global-breadcrumb li:nth-child(1) a").attr("href") should include("/account")
-      document.select("#global-breadcrumb li:nth-child(1) a").text should include("Home")
+      document.select(".breadcrumbs li:nth-child(1) a").attr("href") should include("/account")
+      document.select(".breadcrumbs li:nth-child(1) a").text should include("Home")
 
-      document.select("#global-breadcrumb li:nth-child(2) a").attr("href") should include("/annual-tax-summary")
-      document.select("#global-breadcrumb li:nth-child(2) a").text shouldBe "Select the tax year"
+      document.select(".breadcrumbs li:nth-child(2) a").attr("href") should include("/annual-tax-summary")
+      document.select(".breadcrumbs li:nth-child(2) a").text shouldBe "Select the tax year"
 
-      document.select("#global-breadcrumb li:nth-child(3) a").attr("href") should include(
+      document.select(".breadcrumbs li:nth-child(3) a").attr("href") should include(
         "annual-tax-summary/main?taxYear=2014")
-      document.select("#global-breadcrumb li:nth-child(3) a").text shouldBe "Your Annual Tax Summary"
+      document.select(".breadcrumbs li:nth-child(3) a").text shouldBe "Your Annual Tax Summary"
 
-      document.select("#global-breadcrumb li:nth-child(4) a").attr("href") should include(
+      document.select(".breadcrumbs li:nth-child(4) a").attr("href") should include(
         "/annual-tax-summary/summary?taxYear=2014")
-      document.select("#global-breadcrumb li:nth-child(4) a").text shouldBe "Your income and taxes"
+      document.select(".breadcrumbs li:nth-child(4) a").text shouldBe "Your income and taxes"
 
-      document.select("#global-breadcrumb li:nth-child(5)").toString should include(
-        "<strong>Your Income Tax and National Insurance</strong>")
+      document.select(".breadcrumbs li:nth-child(5)").toString should include("Your Income Tax and National Insurance")
     }
   }
 }
--- test/controllers/SummaryControllerSpec.scala
+++ test/controllers/SummaryControllerSpec.scala
@@ -460,18 +460,17 @@ class SummaryControllerSpec extends ControllerBaseSpec with BeforeAndAfterEach w
       val result = Future.successful(sut.show(request))
       val document = Jsoup.parse(contentAsString(result))
 
-      document.select("#global-breadcrumb li:nth-child(1) a").attr("href") should include("/account")
-      document.select("#global-breadcrumb li:nth-child(1) a").text should include("Home")
+      document.select(".breadcrumbs li:nth-child(1) a").attr("href") should include("/account")
+      document.select(".breadcrumbs li:nth-child(1) a").text should include("Home")
 
-      document.select("#global-breadcrumb li:nth-child(2) a").attr("href") should include("/annual-tax-summary")
-      document.select("#global-breadcrumb li:nth-child(2) a").text shouldBe "Select the tax year"
+      document.select(".breadcrumbs li:nth-child(2) a").attr("href") should include("/annual-tax-summary")
+      document.select(".breadcrumbs li:nth-child(2) a").text shouldBe "Select the tax year"
 
-      document.select("#global-breadcrumb li:nth-child(3) a").attr("href") should include(
+      document.select(".breadcrumbs li:nth-child(3) a").attr("href") should include(
         "/annual-tax-summary/main?taxYear=2014")
-      document.select("#global-breadcrumb li:nth-child(3) a").text shouldBe "Your Annual Tax Summary"
+      document.select(".breadcrumbs li:nth-child(3) a").text shouldBe "Your Annual Tax Summary"
 
-      document.select("#global-breadcrumb li:nth-child(4)").toString should include(
-        "<strong>Your income and taxes</strong>")
+      document.select(".breadcrumbs li:nth-child(4)").toString should include("Your income and taxes")
     }
 
     "Redirect to 'No ATS' page" in {
