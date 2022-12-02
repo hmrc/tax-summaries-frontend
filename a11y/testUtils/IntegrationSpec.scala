5f5d4736a4fa57a52aba7c2590271f44135158a7
Nj147 <47452401+Nj147@users.noreply.github.com>
2022-12-02 15:26:09 +0000

    A11y tests added for 2 home pages (#280)
    * A11y tests added for 2 home pages

---
+++ a11y/testUtils/IntegrationSpec.scala
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package testUtils
+
+import com.github.tomakehurst.wiremock.client.WireMock._
+import config.ApplicationConfig
+import connectors.DataCacheConnector
+import models.{AgentToken, AtsData, AtsListData}
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.i18n.Messages
+import play.api.libs.json.Json
+import play.api.test.Injecting
+import uk.gov.hmrc.domain.Generator
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.cache.client.CacheMap
+import utils.TestConstants.mock
+import utils.WireMockHelper
+import java.time.Instant
+import scala.concurrent.{ExecutionContext, Future}
+
+class IntegrationSpec
+    extends AnyWordSpec
+    with GuiceOneAppPerSuite
+    with Matchers
+    with WireMockHelper
+    with ScalaFutures
+    with IntegrationPatience
+    with Injecting {
+
+  val generatedNino = new Generator().nextNino
+
+  val generatedSaUtr = new Generator().nextAtedUtr
+
+  lazy val ec = inject[ExecutionContext]
+
+  lazy val messages = inject[Messages]
+
+  lazy val appConfig: ApplicationConfig = inject[ApplicationConfig]
+
+  lazy val fakeTaxYear: Int = 2022
+
+  val mockDataCacheConnector = mock[DataCacheConnector]
+
+  val atsListData: AtsListData = Json.fromJson[AtsListData](Json.parse(FileHelper.loadFile("./it/resources/atsList.json"))).get
+
+  val atsData: AtsData = Json.fromJson[AtsData](Json.parse(FileHelper.loadFile(s"./it/resources/atsData_$fakeTaxYear.json"))).get
+
+  val agentTokenMock = AgentToken("uar", generatedSaUtr.utr, Instant.now().toEpochMilli)
+
+  val authResponseNoSA =
+    s"""
+       |{
+       |    "confidenceLevel": 200,
+       |    "nino": "$generatedNino",
+       |    "saUtr": "$generatedSaUtr",
+       |    "name": {
+       |        "name": "John",
+       |        "lastName": "Smith"
+       |    },
+       |    "loginTimes": {
+       |        "currentLogin": "2021-06-07T10:52:02.594Z",
+       |        "previousLogin": null
+       |    },
+       |    "optionalCredentials": {
+       |        "providerId": "4911434741952698",
+       |        "providerType": "GovernmentGateway"
+       |    },
+       |    "authProviderId": {
+       |        "ggCredId": "xyz"
+       |    },
+       |    "externalId": "testExternalId",
+       |    "allEnrolments": []
+       |}
+       |""".stripMargin
+
+  val authResponseSA =
+    s"""
+       |{
+       |    "confidenceLevel": 200,
+       |    "nino": "$generatedNino",
+       |    "saUtr": "$generatedSaUtr",
+       |    "name": {
+       |        "name": "John",
+       |        "lastName": "Smith"
+       |    },
+       |    "loginTimes": {
+       |        "currentLogin": "2021-06-07T10:52:02.594Z",
+       |        "previousLogin": null
+       |    },
+       |    "optionalCredentials": {
+       |        "providerId": "4911434741952698",
+       |        "providerType": "GovernmentGateway"
+       |    },
+       |    "authProviderId": {
+       |        "ggCredId": "xyz"
+       |    },
+       |    "externalId": "testExternalId",
+       |    "allEnrolments": [{
+       |        "key": "IR-SA-AGENT",
+       |        "identifiers": [
+       |          {
+       |            "key": "IRAgentReference",
+       |            "value": "uar"
+       |          }],
+       |        "state": "Activated"
+       |     }]
+       |}
+       |""".stripMargin
+
+
+  override def beforeEach() = {
+    super.beforeEach()
+
+    server.stubFor(
+      put(urlMatching(s"/keystore/tax-summaries-frontend/.*"))
+        .willReturn(ok(Json.toJson(CacheMap("id", Map.empty)).toString))
+    )
+
+    server.stubFor(
+      post(urlEqualTo("/auth/authorise"))
+        .willReturn(ok(authResponseNoSA))
+    )
+
+    when(mockDataCacheConnector.storeAgentToken(any[String])(any[HeaderCarrier], any[ExecutionContext]))
+      .thenReturn(Future.successful("token"))
+
+    when(mockDataCacheConnector.fetchAndGetAtsListForSession(any[HeaderCarrier]))
+      .thenReturn(Future.successful(Some(atsListData)))
+
+    when(mockDataCacheConnector.getAgentToken(any[HeaderCarrier], any()))
+      .thenReturn(Future.successful(Some(agentTokenMock)))
+
+    when(mockDataCacheConnector.storeAtsListForSession(any())(any(), any()))
+      .thenReturn(Future.successful(Some(atsListData)))
+
+    when(mockDataCacheConnector.fetchAndGetAtsForSession(any())(any()))
+      .thenReturn(Future.successful(Some(atsData)))
+
+    when(mockDataCacheConnector.storeAtsForSession(any())(any(), any()))
+      .thenReturn(Future.successful(Some(atsData)))
+  }
+}
---
+++ a11y/views/a11yTestSpec.scala
@@ -0,0 +1,87 @@
+package views
+
+
+import com.github.tomakehurst.wiremock.client.WireMock.{get, ok, urlEqualTo}
+import connectors.DataCacheConnector
+import play.api
+import play.api.Application
+import play.api.http.Status.OK
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.mvc.{AnyContentAsEmpty, Result}
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{GET, contentAsString, defaultAwaitTimeout, route, writeableOf_AnyContentAsEmpty, status => getStatus}
+import testUtils.{FileHelper, IntegrationSpec}
+import uk.gov.hmrc.http.SessionKeys
+import uk.gov.hmrc.scalatestaccessibilitylinter.AccessibilityMatchers
+import uk.gov.hmrc.scalatestaccessibilitylinter.domain.OutputFormat
+
+import java.util.UUID
+import scala.concurrent.Future
+
+class a11yTestSpec extends IntegrationSpec with AccessibilityMatchers {
+
+  lazy val backendUrl = s"/taxs/$generatedSaUtr/$fakeTaxYear/ats-data"
+  lazy val backendUrlSa = s"/taxs/$generatedSaUtr/ats-list"
+
+  override def fakeApplication(): Application = new GuiceApplicationBuilder()
+    .configure(
+      "microservice.services.auth.port"          -> server.port(),
+      "microservice.services.tax-summaries.port" -> server.port(),
+      "microservice.services.cachable.session-cache.port" -> server.port()
+    ).overrides(
+    api.inject.bind[DataCacheConnector].toInstance(mockDataCacheConnector)
+  )
+    .build()
+
+  def request(url: String): FakeRequest[AnyContentAsEmpty.type] = {
+    val uuid = UUID.randomUUID().toString
+    FakeRequest(GET, url).withSession(SessionKeys.sessionId -> uuid)
+  }
+
+  "annual-tax-summary" must {
+    List(
+      "/annual-tax-summary/",
+      "/annual-tax-summary/paye/main"
+    ).foreach { url =>
+      s"pass accessibility validation at url $url" in {
+        server.stubFor(
+          get(urlEqualTo(backendUrlSa))
+            .willReturn(ok(FileHelper.loadFile("./it/resources/atsList.json")))
+        )
+
+        val result: Future[Result] = route(app, request(url)).get
+        getStatus(result) mustBe OK
+        contentAsString(result) must passAccessibilityChecks(OutputFormat.Verbose)
+      }
+    }
+  }
+
+  "annual-tax-summary data pages" must {
+    List(
+      s"/annual-tax-summary/main?taxYear=$fakeTaxYear",
+      s"/annual-tax-summary/summary?taxYear=$fakeTaxYear",
+      s"/annual-tax-summary/nics?taxYear=$fakeTaxYear",
+      s"/annual-tax-summary/treasury-spending?taxYear=$fakeTaxYear",
+      s"/annual-tax-summary/income-before-tax?taxYear=$fakeTaxYear",
+      s"/annual-tax-summary/tax-free-amount?taxYear=$fakeTaxYear",
+      s"/annual-tax-summary/total-income-tax?taxYear=$fakeTaxYear",
+      s"/annual-tax-summary/capital-gains-tax?taxYear=$fakeTaxYear",
+    ).foreach { url =>
+      s"pass accessibility validation at url $url" in {
+        server.stubFor(
+          get(urlEqualTo(backendUrlSa))
+            .willReturn(ok(FileHelper.loadFile("./it/resources/atsList.json")))
+        )
+
+        server.stubFor(
+          get(urlEqualTo(backendUrl))
+            .willReturn(ok(FileHelper.loadFile(s"./it/resources/atsData_$fakeTaxYear.json")))
+        )
+
+        val result: Future[Result] = route(app, request(url)).get
+        getStatus(result) mustBe OK
+        contentAsString(result) must passAccessibilityChecks(OutputFormat.Verbose)
+      }
+    }
+  }
+}
