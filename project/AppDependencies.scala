5459c768d970698bca65b73df4d45f2eea8e7144
Nj147 <nathanjackson147@gmail.com>
2023-02-06 13:50:58 +0000

    DDCNL-6951 Initial setup, connector/service added, conf values added

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,13 +12,14 @@ object AppDependencies {
     "com.typesafe.play"          %% "anorm"                      % "2.5.3",
     "com.typesafe.scala-logging" %% "scala-logging"              % "3.9.2",
     "uk.gov.hmrc"                %% "http-caching-client"        % s"9.5.0-$playVersion",
-    "uk.gov.hmrc"                %% s"bootstrap-frontend-$playVersion" % "5.7.0",
+    "uk.gov.hmrc"                %% s"bootstrap-frontend-$playVersion" % "7.11.0",
     "uk.gov.hmrc"                %% "domain"                     % s"8.1.0-$playVersion",
     "uk.gov.hmrc"                %% "tax-year"                   % "3.0.0",
     "com.mohiva"                 %% "play-html-compressor"       % "0.7.1", // used to pretty print html by stripping out all the whitespaces added by the playframework
     "com.softwaremill.quicklens" %% "quicklens"                  % "1.6.0",
     "org.typelevel"              %% "cats-core"                  % "2.3.1",
-    "uk.gov.hmrc"                %% "play-frontend-hmrc"         % s"3.17.0-play-28"
+    "uk.gov.hmrc"                %% "play-frontend-hmrc"         % s"3.17.0-play-28",
+    "uk.gov.hmrc"           %% "play-partials"                    % s"8.3.0-$playVersion"
   )
 
   val test = Seq(
