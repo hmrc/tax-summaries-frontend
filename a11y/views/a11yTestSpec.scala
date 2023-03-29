47ea012ea1267f6611753d82d0383bc39a531f52
Nj147 <nathanjackson147@gmail.com>
2023-03-29 16:39:19 +0100

    DDCNL-6513 Fixed conf merge error, reverted a11y tests

--- a11y/views/a11yTestSpec.scala
+++ a11y/views/a11yTestSpec.scala
@@ -3,10 +3,8 @@ package views
 
 import com.github.tomakehurst.wiremock.client.WireMock.{get, ok, urlEqualTo}
 import connectors.DataCacheConnector
-import org.mockito.MockitoSugar
 import play.api
 import play.api.Application
-import play.api.cache.AsyncCacheApi
 import play.api.http.Status.OK
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.mvc.{AnyContentAsEmpty, Result}
@@ -20,7 +18,7 @@ import uk.gov.hmrc.scalatestaccessibilitylinter.domain.OutputFormat
 import java.util.UUID
 import scala.concurrent.Future
 
-class a11yTestSpec extends IntegrationSpec with AccessibilityMatchers with MockitoSugar {
+class a11yTestSpec extends IntegrationSpec with AccessibilityMatchers {
 
   lazy val backendUrl = s"/taxs/$generatedSaUtr/$fakeTaxYear/ats-data"
   lazy val backendUrlSa = s"/taxs/$generatedSaUtr/ats-list"
@@ -32,8 +30,7 @@ class a11yTestSpec extends IntegrationSpec with AccessibilityMatchers with Mocki
       "microservice.services.cachable.session-cache.port" -> server.port(),
       "microservice.services.pertax.port" -> server.port()
     ).overrides(
-    api.inject.bind[DataCacheConnector].toInstance(mockDataCacheConnector),
-      api.inject.bind[AsyncCacheApi].toInstance(mock[AsyncCacheApi])
+    api.inject.bind[DataCacheConnector].toInstance(mockDataCacheConnector)
   )
     .build()
 
@@ -93,7 +90,6 @@ class a11yTestSpec extends IntegrationSpec with AccessibilityMatchers with Mocki
         )
 
         val result: Future[Result] = route(app, request(url)).get
-        println("aaaaaaa" + contentAsString(result))
         getStatus(result) mustBe OK
         contentAsString(result) must passAccessibilityChecks(OutputFormat.Verbose)
       }
