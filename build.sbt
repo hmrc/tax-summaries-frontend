53172b867355487817286e558a4311f13fa59100
Christian-QA <62262609+Christian-QA@users.noreply.github.com>
2023-05-18 09:05:48 +0100

    DDCNL-7231: Upgrades (#300)
    * DDCNL-7231: Incremented app deps
    
    * DDCNL-7231: Incremented test deps
    
    * DDCNL-7231: Incremented plugins
    
    * DDCNL-7231: Refactored build
    
    * DDCNL-7231: Decremented sbt to what is in Jenkins

--- build.sbt
+++ build.sbt
@@ -1,6 +1,5 @@
 import sbt._
 import uk.gov.hmrc.DefaultBuildSettings._
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
 import com.typesafe.sbt.web.Import._
 import net.ground5hark.sbt.concat.Import._
 import com.typesafe.sbt.uglify.Import._
@@ -33,7 +32,7 @@ lazy val microservice = Project(appName, file("."))
     scoverageSettings,
     scalaSettings,
     scalaVersion := "2.13.8",
-    publishingSettings,
+    libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always,
     defaultSettings(),
     majorVersion := 1,
     libraryDependencies ++= AppDependencies.all,
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ import play.sbt.PlayImport._
 object AppDependencies {
 
   val playVersion = "play-28"
-  val bootstrapVersion = "7.14.0"
+  val bootstrapVersion = "7.15.0"
   lazy val hmrcMongoVersion: String = "1.1.0"
 
   val compile = Seq(
@@ -15,24 +15,24 @@ object AppDependencies {
     "com.typesafe.scala-logging" %% "scala-logging"              % "3.9.5",
     "uk.gov.hmrc"                %% "http-caching-client"        % s"10.0.0-$playVersion",
     "uk.gov.hmrc"                %% s"bootstrap-frontend-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc"                %% "domain"                     % s"8.1.0-$playVersion",
-    "uk.gov.hmrc"                %% "tax-year"                   % "3.0.0",
+    "uk.gov.hmrc"                %% "domain"                     % s"8.3.0-$playVersion",
+    "uk.gov.hmrc"                %% "tax-year"                   % "3.2.0",
     "org.typelevel"              %% "cats-core"                  % "2.9.0",
-    "uk.gov.hmrc"                %% "play-frontend-hmrc"         % s"6.7.0-play-28",
-    "uk.gov.hmrc"                %% "play-partials"                    % s"8.3.0-$playVersion",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc"         % s"7.7.0-play-28",
+    "uk.gov.hmrc"                %% "play-partials"                    % s"8.4.0-$playVersion",
     "uk.gov.hmrc.mongo"          %% s"hmrc-mongo-$playVersion"   % hmrcMongoVersion,
-    "uk.gov.hmrc"           %% s"internal-auth-client-$playVersion" % "1.2.0",
+    "uk.gov.hmrc"           %% s"internal-auth-client-$playVersion" % "1.4.0",
     "uk.gov.hmrc"           %% "http-caching-client"              % s"10.0.0-$playVersion",
   ehcache
   )
 
   val test               = Seq(
     "uk.gov.hmrc"                %% "play-language"           % s"6.1.0-$playVersion",
-    "org.jsoup"                   % "jsoup"                   % "1.15.4",
+    "org.jsoup"                   % "jsoup"                   % "1.16.1",
     "uk.gov.hmrc"                %% s"bootstrap-test-play-28" % bootstrapVersion,
-    "org.mockito"                %% "mockito-scala-scalatest" % "1.17.12",
+    "org.mockito"                %% "mockito-scala-scalatest" % "1.17.14",
     "com.typesafe.play"          %% "play-test"               % PlayVersion.current,
-    "org.scalatestplus"          %% "scalacheck-1-16"         % "3.2.14.0",
+    "org.scalatestplus"          %% "scalacheck-1-17"         % "3.2.15.0",
     "com.github.tomakehurst"      % "wiremock-jre8"           % "2.35.0",
     "org.pegdown"                 % "pegdown"                 % "1.6.0",
     "com.softwaremill.quicklens" %% "quicklens"               % "1.6.0",
