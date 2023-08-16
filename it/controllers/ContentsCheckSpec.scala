61d35902c32dc9b1664f3de7f4be05033758b478
SandeepUmredkar <sandeep.umredkar@digital.hmrc.gov.uk>
2023-08-16 09:54:50 +0100

    DDCNL-7698

--- it/controllers/ContentsCheckSpec.scala
+++ it/controllers/ContentsCheckSpec.scala
@@ -18,9 +18,7 @@ package controllers
 
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, get, ok, post, urlEqualTo, urlMatching, urlPathMatching}
-import models.admin.{PertaxBackendToggle, SCAWrapperToggle}
 import org.jsoup.Jsoup
-import org.mockito.ArgumentMatchers
 import play.api
 import play.api.Application
 import play.api.http.Status.OK
@@ -30,7 +28,6 @@ import play.api.mvc.{AnyContentAsEmpty, Result}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{GET, contentAsString, defaultAwaitTimeout, route, writeableOf_AnyContentAsEmpty}
 import uk.gov.hmrc.http.SessionKeys
-import uk.gov.hmrc.mongoFeatureToggles.model.FeatureFlag
 import uk.gov.hmrc.mongoFeatureToggles.services.FeatureFlagService
 import uk.gov.hmrc.sca.models.{MenuItemConfig, PtaMinMenuConfig, WrapperDataResponse}
 import utils.{FileHelper, IntegrationSpec}
@@ -46,22 +43,6 @@ class ContentsCheckSpec extends IntegrationSpec {
 
   def getExpectedData(key: String): ExpectedData =
     key match {
-      case "main"              =>
-        ExpectedData("Service unavailable - Personal tax account - GOV.UK")
-      case "summary"           =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
-      case "nics"              =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
-      case "treasury-spending" =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
-      case "income-before-tax" =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
-      case "tax-free-amount"   =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
-      case "total-income-tax"  =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
-      case "capital-gains-tax" =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
 
       case "not-authorised"           =>
         ExpectedData("We could not confirm your identity - Annual Tax Summary - GOV.UK")
@@ -69,16 +50,8 @@ class ContentsCheckSpec extends IntegrationSpec {
         ExpectedData("Bad request - 400 - Annual Tax Summary - GOV.UK")
       case "service-unavailable"      =>
         ExpectedData("Sorry there is a problem with the service - Annual Tax Summary - GOV.UK")
-      case "sign-out"                 =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
       case "paye-year"                =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
-      case "paye-main"                =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
-      case "paye-main-year"           =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
-      case "upliftConfidenceLevel"    =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
+        ExpectedData("How your tax was spent: 2021 to 2022 - Annual Tax Summary - GOV.UK")
       case "paye-summary-year"        =>
         ExpectedData("Your income and taxes: 2021 to 2022 - Annual Tax Summary - GOV.UK")
       case "income-insurance-year"    =>
@@ -87,12 +60,10 @@ class ContentsCheckSpec extends IntegrationSpec {
         ExpectedData("Taxable income: 2021 to 2022 - Annual Tax Summary - GOV.UK")
       case "tax-free-amount-year"     =>
         ExpectedData("Tax-free amount: 2021 to 2022 - Annual Tax Summary - GOV.UK")
-      case "not-authorised"           =>
+      case "paye-not-authorised"      =>
         ExpectedData("We could not confirm your identity - Annual Tax Summary - GOV.UK")
       case "paye-service-unavailable" =>
         ExpectedData("Sorry there is a problem with the service - Annual Tax Summary - GOV.UK")
-      case "keepalive"                =>
-        ExpectedData("Sign in - Personal tax account - GOV.UK")
       case "session-expired"          =>
         ExpectedData("For your security, we signed you out - Annual Tax Summary - GOV.UK")
       case "/"                        =>
@@ -105,87 +76,20 @@ class ContentsCheckSpec extends IntegrationSpec {
 
   val urls: Map[String, ExpectedData] = Map(
     "/annual-tax-summary"                                             -> getExpectedData("/"),
-    "/annual-tax-summary/main"                                        -> getExpectedData("main"),
-    "/annual-tax-summary/summary"                                     -> getExpectedData("summary"),
-    "/annual-tax-summary/nics"                                        -> getExpectedData("nics"),
-    "/annual-tax-summary/treasury-spending"                           -> getExpectedData("treasury-spending"),
-    "/annual-tax-summary/income-before-tax"                           -> getExpectedData("income-before-tax"),
-    "/annual-tax-summary/tax-free-amount"                             -> getExpectedData("tax-free-amount"),
-    "/annual-tax-summary/total-income-tax"                            -> getExpectedData("total-income-tax"),
-    "/annual-tax-summary/capital-gains-tax"                           -> getExpectedData("capital-gains-tax"),
-    //"/annual-tax-summary/not-authorised"                              -> getExpectedData("not-authorised"),
-    //"/annual-tax-summary/no-ats"                                      -> getExpectedData("no-ats"),
-    //"/annual-tax-summary/service-unavailable"                         -> getExpectedData("service-unavailable"),
-    "/annual-tax-summary/sign-out"                                    -> getExpectedData("sign-out"),
+    "/annual-tax-summary/not-authorised"                              -> getExpectedData("not-authorised"),
+    "/annual-tax-summary/no-ats"                                      -> getExpectedData("no-ats"),
+    "/annual-tax-summary/service-unavailable"                         -> getExpectedData("service-unavailable"),
     "/annual-tax-summary/paye/treasury-spending/2022"                 -> getExpectedData("paye-year"),
-    "/annual-tax-summary/paye/main"                                   -> getExpectedData("paye-main"),
-    "/annual-tax-summary/paye/main/2022 "                             -> getExpectedData("paye-main-year"),
-    "/annual-tax-summary/upliftConfidenceLevel"                       -> getExpectedData("upliftConfidenceLevel"),
     "/annual-tax-summary/paye/summary/2022"                           -> getExpectedData("paye-summary-year"),
     "/annual-tax-summary/paye/income-tax-and-national-insurance/2022" -> getExpectedData("income-insurance-year"),
     "/annual-tax-summary/paye/income-before-tax/2022"                 -> getExpectedData("income-before-tax-year"),
     "/annual-tax-summary/paye/tax-free-amount/2022"                   -> getExpectedData("tax-free-amount-year"),
-    //"/annual-tax-summary/paye/not-authorised"                         -> getExpectedData("not-authorised"),
-    //"/annual-tax-summary/paye/service-unavailable"                    -> getExpectedData("paye-service-unavailable"),
-    "/annual-tax-summary/keepalive"                                   -> getExpectedData("keepalive"),
+    "/annual-tax-summary/paye/not-authorised"                         -> getExpectedData("paye-not-authorised"),
+    "/annual-tax-summary/paye/service-unavailable"                    -> getExpectedData("paye-service-unavailable"),
     "/annual-tax-summary/session-expired"                             -> getExpectedData("session-expired")
   )
 
-  val messageCount: Int                    = Random.between(1, 100)
-  val menuWrapperData: Seq[MenuItemConfig] = Seq(
-    MenuItemConfig(
-      "home",
-      "Account home",
-      "/home",
-      leftAligned = true,
-      position = 0,
-      Some("hmrc-account-icon hmrc-account-icon--home"),
-      None
-    ),
-    MenuItemConfig(
-      "messages",
-      "Messages",
-      "/personal-account/messages",
-      leftAligned = false,
-      position = 0,
-      None,
-      None
-    ),
-    MenuItemConfig(
-      "progress",
-      "progress",
-      "/track",
-      leftAligned = false,
-      position = 1,
-      None,
-      None
-    ),
-    MenuItemConfig(
-      "profile",
-      "Profile and settings",
-      "/personal-account/profile-and-settings",
-      leftAligned = false,
-      position = 2,
-      None,
-      None
-    ),
-    MenuItemConfig(
-      "signout",
-      "Sign out",
-      "/personal-account/sign-out",
-      leftAligned = false,
-      position = 4,
-      None,
-      None
-    )
-  )
-  val wrapperDataResponse: String          = Json
-    .toJson(
-      WrapperDataResponse(menuWrapperData, PtaMinMenuConfig("MenuName", "BackName"))
-    )
-    .toString
-
-  val tcsBrokerUrl = s"/tcs/$generatedNino/dashboard-data"
+  val messageCount: Int = Random.between(1, 100)
 
   val citizenResponse: String =
     s"""|
@@ -238,14 +142,20 @@ class ContentsCheckSpec extends IntegrationSpec {
        |}
        |""".stripMargin
 
+  val wrapperDataResponse: String = Json
+    .toJson(
+      WrapperDataResponse(
+        Seq(
+          MenuItemConfig("id", "NewLayout Item", "link", leftAligned = false, 0, None, None),
+          MenuItemConfig("signout", "Sign out", "link", leftAligned = false, 0, None, None)
+        ),
+        PtaMinMenuConfig("MenuName", "BackName")
+      )
+    )
+    .toString
+
   override def beforeEach(): Unit = {
     super.beforeEach()
-    reset(mockFeatureFlagService)
-    when(mockFeatureFlagService.get(ArgumentMatchers.eq(SCAWrapperToggle))) thenReturn Future.successful(
-      FeatureFlag(SCAWrapperToggle, isEnabled = true)
-    )
-    when(mockFeatureFlagService.get(ArgumentMatchers.eq(PertaxBackendToggle)))
-      .thenReturn(Future.successful(FeatureFlag(PertaxBackendToggle, isEnabled = false)))
 
     server.stubFor(
       get(urlPathMatching(s"$cacheMap/.*"))
@@ -254,20 +164,20 @@ class ContentsCheckSpec extends IntegrationSpec {
             .withStatus(OK)
             .withBody("""
                 |{
-                |	"id": "session-id",
-                |	"data": {
+                | "id": "session-id",
+                | "data": {
                 |   "addressPageVisitedDto": {
                 |     "hasVisitedPage": true
                 |   }
-                |	},
-                |	"modifiedDetails": {
-                |		"createdAt": {
-                |			"$date": 1400258561678
-                |		},
-                |		"lastUpdated": {
-                |			"$date": 1400258561675
-                |		}
-                |	}
+                | },
+                | "modifiedDetails": {
+                |    "createdAt": {
+                |       "$date": 1400258561678
+                |    },
+                |    "lastUpdated": {
+                |       "$date": 1400258561675
+                |    }
+                | }
                 |}
                 |""".stripMargin)
         )
@@ -306,10 +216,6 @@ class ContentsCheckSpec extends IntegrationSpec {
       get(urlEqualTo(s"/taxs//2022/4/ats-list"))
         .willReturn(ok(FileHelper.loadFile("./it/resources/atsList.json")))
     )
-
-    //TODO
-    server.stubFor(get(urlEqualTo("/messages/count?read=No")).willReturn(ok(s"""{"count": $messageCount}""")))
-
   }
 
   override implicit lazy val app: Application = new GuiceApplicationBuilder()
@@ -340,14 +246,13 @@ class ContentsCheckSpec extends IntegrationSpec {
           val result: Future[Result] = route(app, request(url)).get
           val content                = Jsoup.parse(contentAsString(result))
 
-          //status(result) mustBe OK
           content.title() mustBe expectedData.title
 
-          //val govUkBanner = content.getElementsByClass("govuk-phase-banner")
-          //govUkBanner.size() mustBe 1
-          // govUkBanner.get(0).getElementsByClass("govuk-link").get(0).attr("href") must include(
-          //  "http://localhost:9250/contact/beta-feedback-unauthenticated?service=ATS"
-          // )
+          val govUkBanner = content.getElementsByClass("govuk-phase-banner")
+          govUkBanner.size() mustBe 1
+          govUkBanner.get(0).getElementsByClass("govuk-link").get(0).attr("href") must include(
+            "http://localhost:9250/contact/beta-feedback-unauthenticated?service=ATS"
+          )
 
           val accessibilityStatement = content
             .getElementsByClass("govuk-footer__link")
@@ -360,31 +265,11 @@ class ContentsCheckSpec extends IntegrationSpec {
             "http://localhost:12346/accessibility-statement/annual-tax-summary?referrerUrl"
           )
 
-          /*val urBannerLink = content
-            .getElementsByClass("hmrc-user-research-banner__link")
+          val urBannerLink = content
+            .getElementsByClass("govuk-link hmrc-user-research-banner__link")
             .get(0)
             .attr("href")
-          if (url.equals("/personal-account/child-benefit/home")) {
-            urBannerLink mustBe "https://docs.google.com/forms/d/e/1FAIpQLSegbiz4ClGW0XkC1pY3B02ltiY1V79V7ha0jZinECIz_FvSyg/viewform"
-          } else {
-            urBannerLink mustBe "https://signup.take-part-in-research.service.gov.uk/home?utm_campaign=PTAhomepage&utm_source=Other&utm_medium=other&t=HMRC&id=209"
-          }*/
-
-          /*val signoutLink = content
-            .getElementsByClass("hmrc-account-menu__link")
-            .asScala
-            .toList
-            .find(_.html().contains("Sign out"))
-            .get
-            .attr("href")
-          signoutLink mustBe "/personal-account/signout?continueUrl=http%3A%2F%2Flocalhost%3A9514%2Ffeedback%2FPERTAX"*/
-
-          //val displayedMessageCount = content.getElementsByClass("hmrc-notification-badge").get(0).text()
-          //displayedMessageCount mustBe messageCount.toString
-
-          val menuItems = content.getElementsByClass("hmrc-account-menu")
-          for (menuItem <- menuWrapperData)
-            menuItems.text() must include(menuItem.text)
+          urBannerLink mustBe "https://signup.take-part-in-research.service.gov.uk/?utm_campaign=Ats_FPOS&utm_source=Survey_Banner&utm_medium=other&t=HMRC&id=128"
 
           val languageToggle = content.getElementsByClass("hmrc-language-select__list")
           languageToggle.text() must include("English")
