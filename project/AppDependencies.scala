bbfe5341c3bedf5636c653fe5f1285c2f94f0a6f
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2024-01-11 14:52:53 +0000

    fixed review comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,7 +14,7 @@ object AppDependencies {
     ws,
     "com.typesafe.scala-logging" %% "scala-logging"              % "3.9.5",
     "uk.gov.hmrc"                %% s"http-caching-client-$playVersion"        % s"11.1.0",
-    "uk.gov.hmrc"                %% "tax-year"                   % "3.2.0",
+    "uk.gov.hmrc"                %% "tax-year"                   % "4.0.0",
     "org.typelevel"              %% "cats-core"                  % "2.10.0",
     "uk.gov.hmrc"               %% s"mongo-feature-toggles-client-$playVersion"     % "1.1.0",
     "uk.gov.hmrc" %% s"sca-wrapper-$playVersion" % "1.1.0"
@@ -23,7 +23,7 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "org.jsoup"                   % "jsoup"                   % "1.16.1",
     "uk.gov.hmrc"                %% s"bootstrap-test-$playVersion" % bootstrapVersion,
-    "org.mockito"       %% "mockito-scala-scalatest" % "1.17.29",
+    "org.mockito"       %% "mockito-scala-scalatest" % "1.17.30",
     "org.scalatestplus"          %% "scalacheck-1-17"         % "3.2.17.0",
     "com.softwaremill.quicklens" %% "quicklens"               % "1.9.6"
   ).map(_ % "test")
