c880ba58cc50b75dbd8e082e0b8addd4cf802126
grantrobson <grant.robson@digital.hmrc.gov.uk>
2024-02-20 11:55:24 +0000

    8471 WIP

--- app/controllers/auth/actions/SelfAssessmentAction.scala
+++ app/controllers/auth/actions/SelfAssessmentAction.scala
@@ -37,7 +37,6 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class SelfAssessmentActionImpl @Inject() (
   citizenDetailsService: CitizenDetailsService,
-  ninoAuthAction: NinoAuthAction,
   appConfig: ApplicationConfig,
   override val authConnector: DefaultAuthConnector,
   cc: MessagesControllerComponents
@@ -49,159 +48,70 @@ class SelfAssessmentActionImpl @Inject() (
   override val parser: BodyParser[AnyContent]               = cc.parsers.defaultBodyParser
   override protected val executionContext: ExecutionContext = cc.executionContext
 
-  val saShuttered: Boolean                                                                                           = appConfig.saShuttered
+  private val saShuttered: Boolean = appConfig.saShuttered
+
   override def invokeBlock[A](request: Request[A], block: AuthenticatedRequest[A] => Future[Result]): Future[Result] =
     if (saShuttered) {
       Future.successful(Redirect(controllers.routes.ErrorController.serviceUnavailable))
     } else {
-      implicit val hc: HeaderCarrier =
-        HeaderCarrierConverter.fromRequestAndSession(request, request.session)
-
-// Below is what is in pertax backend authorise:-
-//def authorise(): Action[AnyContent] = authenticate.async { implicit request =>
-//  implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequest(request.request)
-//  request.version match {
-//    case "2.0" =>
-//      val responseF = (for {
-//        _ <- authorisationUtils.checkAffinity(request)
-//        _ <- authorisationUtils.checkConfidenceLevel(request)
-//        _ <- authorisationUtils.checkCredentialStrength(request)
-//        _ <- authorisationUtils.checkDesignatoryDetails(request)
-//        _ <- enrolmentChecks.checkSingleAccountEnrolment(request)
-//      } yield ApiResponse("ACCESS_GRANTED", "Access granted", reportAs = OK)).merge
-//
-//      responseF.map(response => Status(response.reportAs)(response))
-//
-//    case _ =>
-//      Future.successful(
-//        ApiResponse("INVALID_API_VERSION", "Invalid version of the API", reportAs = NOT_IMPLEMENTED).toResult
-//      )
-//  }
-//}
-
-// Below is the original auth action:-
-      println("\nAA1")
-      authorised(ConfidenceLevel.L50)
-        .retrieve(
-          Retrievals.allEnrolments and Retrievals.externalId and Retrievals.credentials and Retrievals.saUtr and Retrievals.confidenceLevel
-        ) {
-          case Enrolments(enrolments) ~ Some(externalId) ~ Some(credentials) ~ saUtr ~ confidenceLevel =>
-            println("\nAA2")
-            val agentRef: Option[Uar] = enrolments.find(_.key == "IR-SA-AGENT").flatMap { enrolment =>
-              enrolment.identifiers
-                .find(id => id.key == "IRAgentReference")
-                .map(key => Uar(key.value))
-            }
-
-            val isAgentActive: Boolean = enrolments.find(_.key == "IR-SA-AGENT").exists(_.isActivated)
-
-            val rq = requests.AuthenticatedRequest(
-              externalId,
-              agentRef,
-              saUtr.map(s => SaUtr(s)),
-              None,
-              saUtr.isDefined,
-              isAgentActive,
-              confidenceLevel,
-              credentials,
-              request
+      implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
+      authorised(ConfidenceLevel.L50).retrieve(
+        Retrievals.allEnrolments and Retrievals.externalId and Retrievals.credentials and Retrievals.saUtr and Retrievals.confidenceLevel
+      ) {
+        case Enrolments(enrolments) ~ Some(externalId) ~ Some(credentials) ~ saUtr ~ confidenceLevel =>
+          val (agentRef, isAgentActive) = agentInfo(enrolments)
+          processAuth(agentRef, isAgentActive, saUtr) { newSaUtr =>
+            block(
+              requests.AuthenticatedRequest(
+                userId = externalId,
+                agentRef = agentRef,
+                saUtr = newSaUtr,
+                nino = None,
+                isSa = saUtr.isDefined,
+                isAgentActive = isAgentActive,
+                confidenceLevel = confidenceLevel,
+                credentials = credentials,
+                request = request
+              )
             )
-
-            implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
-// Below is the original selfassessmentaction
-
-            (agentRef, isAgentActive, saUtr) match {
-              case (Some(_), false, _) => Future.successful(Redirect(controllers.routes.ErrorController.notAuthorised))
-              case (None, _, _) => // Not an agent: get the nino:-
-                ninoAuthAction.getNino().flatMap { atsNinoResponse =>
-                  println("\natsNinoResponse=" + atsNinoResponse)
-                  handleResponse(rq, atsNinoResponse).flatMap {
-                    case Left(r)  => Future.successful(r)
-                    case Right(r) => block(r)
-                  }
-                }
-
-
-//
-//                val x = authorised(ConfidenceLevel.L200 and CredentialStrength(CredentialStrength.strong)).retrieve(Retrievals.nino) {
-//                  case Some(nino) => Future(SuccessAtsNino(nino))
-//                  case _          => Future(NoAtsNinoFound)
-//                } recover {
-//                  case _: InsufficientConfidenceLevel => UpliftRequiredAtsNino
-//                  case _: IncorrectCredentialStrength => InsufficientCredsNino
-//                }
-              
-              
-              
-              case _ => block(rq)
-            }
-            
-            
-//            if (agentRef.isDefined && !isAgentActive) {
-//              println("\n**** IS AGENT")
-//              Future.successful(Redirect(controllers.routes.ErrorController.notAuthorised))
-//            } else if (saUtr.isEmpty && agentRef.isEmpty) {
-//              println("\n**** IS NOT AGENT 1")
-//              ninoAuthAction.getNino().flatMap { atsNinoResponse =>
-//                println("\natsNinoResponse=" + atsNinoResponse)
-//                handleResponse(rq, atsNinoResponse).flatMap {
-//                  case Left(r)  => Future.successful(r)
-//                  case Right(r) => block(r)
-//                }
-//              }
-//            } else {
-//              println("\n**** IS NOT AGENT - 2" + saUtr + "/" + agentRef)
-//              block(rq)
-//            }
-          case _                                                                                       => throw new RuntimeException("Can't find credentials for user")
-        }
-    } recover {
-      case _: NoActiveSession =>
-        lazy val ggSignIn    = appConfig.loginUrl
-        lazy val callbackUrl = appConfig.loginCallback
-        Redirect(
-          ggSignIn,
-          Map(
-            "continue_url" -> Seq(callbackUrl),
-            "origin"       -> Seq(appConfig.appName)
+          }
+        case _                                                                                       => throw new RuntimeException("Can't find credentials for user")
+      } recover {
+        case _: NoActiveSession        =>
+          Redirect(
+            appConfig.loginUrl,
+            Map(
+              "continue_url" -> Seq(appConfig.loginCallback),
+              "origin"       -> Seq(appConfig.appName)
+            )
           )
-        )
-
-      case _: InsufficientEnrolments => Redirect(controllers.routes.ErrorController.notAuthorised)
+        case _: InsufficientEnrolments => Redirect(controllers.routes.ErrorController.notAuthorised)
+      }
     }
 
-  private def handleResponse[T](request: AuthenticatedRequest[T], atsNinoResponse: AtsNino)(implicit
-    hc: HeaderCarrier
-  ): Future[Either[Result, AuthenticatedRequest[T]]] =
-    atsNinoResponse match {
-      case SuccessAtsNino(nino)  =>
-        citizenDetailsService.getMatchingDetails(nino).map {
-          case SucccessMatchingDetailsResponse(value) =>
-            if (value.saUtr.isDefined) { Right(createAuthenticatedRequest(request, value.saUtr)) }
-            else {
-              Left(Redirect(controllers.routes.ErrorController.notAuthorised))
-            }
-          case _                                      =>
-            Left(
-              Redirect(controllers.routes.ErrorController.notAuthorised)
-            )
-        }
-      case NoAtsNinoFound        =>
-        println("\nNO NINO FOUND REDIRECT")
-        Future(
-          Left(
-            Redirect(controllers.routes.ErrorController.notAuthorised)
-          )
-        )
-      case InsufficientCredsNino =>
-        Future(
-          Left(
-            Redirect(controllers.routes.ErrorController.notAuthorised)
-          )
-        )
-      case UpliftRequiredAtsNino =>
-        Future(
-          Left(
+  private def processAuth(agentRef: Option[Uar], isAgentActive: Boolean, saUtr: Option[String])(
+    block: Option[SaUtr] => Future[Result]
+  )(implicit hc: HeaderCarrier): Future[Result] =
+    (agentRef, isAgentActive, saUtr) match {
+      case (Some(_), false, _) => Future.successful(Redirect(controllers.routes.ErrorController.notAuthorised))
+      case (None, _, _)        =>
+        authorised(ConfidenceLevel.L200 and CredentialStrength(CredentialStrength.strong))
+          .retrieve(Retrievals.nino) {
+            case Some(nino) =>
+              citizenDetailsService.getMatchingDetails(nino).flatMap {
+                case SucccessMatchingDetailsResponse(value) =>
+                  if (value.saUtr.isDefined) {
+                    //              saUtr.map(s => SaUtr(s)),
+                    block(value.saUtr)
+                  } else {
+                    Future.successful(Redirect(controllers.routes.ErrorController.notAuthorised))
+                  }
+                case _                                      =>
+                  Future.successful(Redirect(controllers.routes.ErrorController.notAuthorised))
+              }
+            case _          => Future(Redirect(controllers.routes.ErrorController.notAuthorised))
+          } recover {
+          case _: InsufficientConfidenceLevel =>
             Redirect(
               appConfig.identityVerificationUpliftUrl,
               Map(
@@ -211,25 +121,29 @@ class SelfAssessmentActionImpl @Inject() (
                 "failureURL"      -> Seq(appConfig.iVUpliftFailureCallback)
               )
             )
-          )
-        )
+
+          case _: IncorrectCredentialStrength =>
+            Redirect(controllers.routes.ErrorController.notAuthorised)
+        }
     }
 
-  private def createAuthenticatedRequest[T](
-    request: AuthenticatedRequest[T],
-    newSaUtr: Option[SaUtr]
-  ): AuthenticatedRequest[T] =
-    requests.AuthenticatedRequest(
-      userId = request.userId,
-      agentRef = request.agentRef,
-      saUtr = newSaUtr,
-      nino = request.nino,
-      isSa = request.isSa,
-      isAgentActive = request.isAgentActive,
-      confidenceLevel = request.confidenceLevel,
-      credentials = request.credentials,
-      request = request
-    )
+  private def agentInfo(enrolments: Set[Enrolment]): (Option[Uar], Boolean) =
+    enrolments
+      .find(_.key == "IR-SA-AGENT")
+      .map { enrolment =>
+        val d = enrolment.identifiers
+          .find(id => id.key == "IRAgentReference")
+          .map(key => Uar(key.value))
+        Tuple2(d, enrolment.isActivated)
+      }
+      .getOrElse(None, false)
+  //
+  //            val agentRef: Option[Uar] = enrolments.find(_.key == "IR-SA-AGENT").flatMap { enrolment =>
+  //              enrolment.identifiers
+  //                .find(id => id.key == "IRAgentReference")
+  //                .map(key => Uar(key.value))
+  //            }
+  //            val isAgentActive: Boolean = enrolments.find(_.key == "IR-SA-AGENT").exists(_.isActivated)
 }
 
 @ImplementedBy(classOf[SelfAssessmentActionImpl])
