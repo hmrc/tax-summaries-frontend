bb44cd9f5b105518a66d494d17ee9825c3970655
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2025-02-07 11:15:02 +0000

    DDCNL-9856: Updated dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,23 +4,23 @@ import sbt.*
 object AppDependencies {
 
   private val playVersion          = "play-30"
-  private val scaWrapperVersion    = "2.1.0"
-  private val featureToggleVersion = "1.8.0"
+  private val scaWrapperVersion    = "2.3.0"
+  private val featureToggleVersion = "1.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     filters,
     ws,
     "uk.gov.hmrc"   %% "tax-year"                                   % "5.0.0",
-    "org.typelevel" %% "cats-core"                                  % "2.12.0",
+    "org.typelevel" %% "cats-core"                                  % "2.13.0",
     "uk.gov.hmrc"   %% s"mongo-feature-toggles-client-$playVersion" % featureToggleVersion,
     "uk.gov.hmrc"   %% s"sca-wrapper-$playVersion"                  % scaWrapperVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.jsoup"          % "jsoup"                                           % "1.18.1",
+    "org.jsoup"          % "jsoup"                                           % "1.18.3",
     "uk.gov.hmrc"       %% s"mongo-feature-toggles-client-test-$playVersion" % featureToggleVersion,
     "uk.gov.hmrc"       %% s"sca-wrapper-test-$playVersion"                  % scaWrapperVersion,
-    "org.scalatestplus" %% "scalacheck-1-17"                                 % "3.2.18.0"
+    "org.scalatestplus" %% "scalacheck-1-18"                                 % "3.2.19.0"
   ).map(_ % "test")
 
   val all: Seq[ModuleID]  = compile ++ test
