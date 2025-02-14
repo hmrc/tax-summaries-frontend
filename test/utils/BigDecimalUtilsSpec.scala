0e6c9d480a331234f2011166f80e008a1ac27c18
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2025-02-14 09:23:40 +0000

    DDCNL-9856: Updated spec to use matcher

--- test/utils/BigDecimalUtilsSpec.scala
+++ test/utils/BigDecimalUtilsSpec.scala
@@ -29,8 +29,8 @@ class BigDecimalUtilsSpec extends AnyWordSpec with BigDecimalUtils with Matchers
       "two big decimals are exactly equal" in {
 
         forAll { (bd: BigDecimal) =>
-          assert(bd === bd)
-          assert(!(bd !== bd))
+          (bd === bd) mustBe true
+          (bd !== bd) mustBe false
         }
       }
     }
@@ -40,9 +40,9 @@ class BigDecimalUtilsSpec extends AnyWordSpec with BigDecimalUtils with Matchers
       "two big decimals are not the same" in {
 
         forAll { (bd1: BigDecimal, bd2: BigDecimal) =>
-          if (bd1 !== bd2) {
-            assert(!(bd1 === bd2))
-            assert(bd1 !== bd2)
+          whenever(bd1 !== bd2) {
+            (bd1 === bd2) mustBe false
+            (bd1 !== bd2) mustBe true
           }
         }
       }
